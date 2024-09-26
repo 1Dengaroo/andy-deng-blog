bfb67adbaceee25b0ef43199e2fd64ee036c7e23
Andy Deng <andydengaroo@gmail.com>
2024-09-26 15:11:13 -0400

    Fix font styling

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,14 @@ export default function Page() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Andy H. Deng
       </h1>
-      <p className="mb-6 text-lg">
+      <p className="mb-6">
         {`Hello, my name is Andy Deng and I'm a recent computer science graduate from Boston College. 
         I have the skills of a software engineer, a web developer, and a game developer. 
         I'm actively involved in developing e-commerce platforms and gaming applications. 
         Recently, I've been working on a project to develop a web-based game using Unity and C#. 
         I'm also working as a contractor to help migrate an e-commerce platform from React and Rails to Next.js, Medusa, and Node.js.`}
       </p>
-      <p className="mb-6 text-lg">
+      <p className="mb-6">
         {`I have completed multiple internships at well-respected and established companies. 
         At Vanta, I improved my software development skills working as a full stack software engineer. 
         Concurrently, I worked with Professor Maira Samary at Boston College to develop a TA-Professor matching system. 
